ef9c9c54a3379f1e2628fa8c0aa84f3dccd2d75b
LAPTOP-LF28MDJ2\yassi <LAARIBI_LAHLOUYASSINE@emsi-edu.ma>
2022-11-13 13:23:15 +0100

    FC1

---
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/web/BankAccountGraphQLController.java
@@ -0,0 +1,40 @@
+package org.sid.bankaccountservice.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BankAccountGraphQLController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @QueryMapping
+    public List<BankAccount> accountsList(){
+        return bankAccountRepository.findAll();
+    }
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id){
+        return bankAccountRepository.findById(id)
+                .orElseThrow(()->new RuntimeException(String.format("Account %s not found",id)));
+    }
+    @MutationMapping
+    public BankAccount addAcount(@Argument BankAccount bankAccount){
+        return bankAccountRepository.save(bankAccount);
+    }
+}
+@Data @NoArgsConstructor @AllArgsConstructor
+class BankAccountDTO {
+    private String type;
+    private Double balance;
+    private String currency;
+
+}
